be048ca3a77e4827ef36942fdaac379c29b9b050
soewanna26 <mgsoewana26@gmail.com>
2024-06-04 11:25:58 +0630

    Update

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,32 +1,32 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from "resend";
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-export async function POST(req, res) {
-  const { body } =await req.json();
-  const { email, subject, message } = body;
-  try {
-    const { data, error } = await resend.emails.send({
-      from: fromEmail,
-      to: ["mgsoewana26@gmail.com"],
-      subject: "CV Form",
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New Message Submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
+// export async function POST(req, res) {
+//   const { body } =await req.json();
+//   const { email, subject, message } = body;
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: fromEmail,
+//       to: ["mgsoewana26@gmail.com"],
+//       subject: "CV Form",
+//       react: (
+//         <>
+//           <h1>{subject}</h1>
+//           <p>Thank you for contacting us!</p>
+//           <p>New Message Submitted:</p>
+//           <p>{message}</p>
+//         </>
+//       ),
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,115 +1,115 @@
-"use client";
-import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
-const EmailSection = () => {
-    const [emailSubmitted,setEmailSubmitted] = useState(false);
-    const handleSubmit = async(e) =>{
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        }
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
+// "use client";
+// import React, { useState } from "react";
+// import GithubIcon from "../../../public/github-icon.svg";
+// import Link from "next/link";
+// import Image from "next/image";
+// const EmailSection = () => {
+//     const [emailSubmitted,setEmailSubmitted] = useState(false);
+//     const handleSubmit = async(e) =>{
+//         e.preventDefault();
+//         const data = {
+//             email: e.target.email.value,
+//             subject: e.target.subject.value,
+//             message: e.target.message.value,
+//         }
+//         const JSONdata = JSON.stringify(data);
+//         const endpoint = "/api/send";
 
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSONdata,
-        }
+//         const options = {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSONdata,
+//         }
 
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
-        console.log(resData);
-        if(response.status === 200){
-            console.log('Message sent successfully');
-            setEmailSubmitted(true);
-        }
-    }
-  return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
-        <div className="absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg top-3/4 -left-4 transform -translate-x-1"></div>
-      <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I&apos;m currently looking for new opportunites, my inbox is always
-          open.Whether you have a question or just want to say hi, I&apos;ll try
-          my best to get back to you!
-        </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/soewanna26?tab=repositories">
-            <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
-        </div>
-      </div>
-      <div>
-        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="text-white block mb-2 text-sm font-medium"
-            >
-              Your Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              required
-              placeholder="abc@gmail.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className="text-white block mb-2 text-sm font-medium"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              required
-              placeholder="Just Say hi"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="text-white block mb-2 text-sm font-medium"
-            >
-              Message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Let's talk about...."
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-          >Send Message</button>
-          {
-            emailSubmitted && (
-                <p className="text-green-500 text-sm mt-2">Email sent Successfully!</p>
-            )
-          }
-        </form>
-      </div>
-    </section>
-  );
-};
+//         const response = await fetch(endpoint, options);
+//         const resData = await response.json();
+//         console.log(resData);
+//         if(response.status === 200){
+//             console.log('Message sent successfully');
+//             setEmailSubmitted(true);
+//         }
+//     }
+//   return (
+//     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+//         <div className="absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg top-3/4 -left-4 transform -translate-x-1"></div>
+//       <div className="z-10">
+//         <h5 className="text-xl font-bold text-white my-2">
+//           Let&apos;s Connect
+//         </h5>
+//         <p className="text-[#ADB7BE] mb-4 max-w-md">
+//           I&apos;m currently looking for new opportunites, my inbox is always
+//           open.Whether you have a question or just want to say hi, I&apos;ll try
+//           my best to get back to you!
+//         </p>
+//         <div className="socials flex flex-row gap-2">
+//           <Link href="https://github.com/soewanna26?tab=repositories">
+//             <Image src={GithubIcon} alt="Github Icon" />
+//           </Link>
+//         </div>
+//       </div>
+//       <div>
+//         <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+//           <div className="mb-6">
+//             <label
+//               htmlFor="email"
+//               className="text-white block mb-2 text-sm font-medium"
+//             >
+//               Your Email
+//             </label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               required
+//               placeholder="abc@gmail.com"
+//             />
+//           </div>
+//           <div className="mb-6">
+//             <label
+//               htmlFor="subject"
+//               className="text-white block mb-2 text-sm font-medium"
+//             >
+//               Subject
+//             </label>
+//             <input
+//               type="text"
+//               id="subject"
+//               name="subject"
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               required
+//               placeholder="Just Say hi"
+//             />
+//           </div>
+//           <div className="mb-6">
+//             <label
+//               htmlFor="message"
+//               className="text-white block mb-2 text-sm font-medium"
+//             >
+//               Message
+//             </label>
+//             <textarea
+//               name="message"
+//               id="message"
+//               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//               placeholder="Let's talk about...."
+//             ></textarea>
+//           </div>
+//           <button
+//             type="submit"
+//             className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+//           >Send Message</button>
+//           {
+//             emailSubmitted && (
+//                 <p className="text-green-500 text-sm mt-2">Email sent Successfully!</p>
+//             )
+//           }
+//         </form>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default EmailSection;
+// export default EmailSection;
